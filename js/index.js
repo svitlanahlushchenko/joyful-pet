adf7d418d9645b63a97e3ce784500c05879bebe2
Svitlana <shumovasveta27@gmail.com>
2024-10-01 14:26:07 +0200

    slider fixed

--- js/index.js
+++ js/index.js
@@ -20,6 +20,9 @@ const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 20,
   loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
